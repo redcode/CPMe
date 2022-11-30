9a5b3f1f1df61ee96f8f5056e1728c2c42675300
redcode <manuel@zxe.io>
2022-11-30 15:06:01 +0100

    First commit.

---
+++ sources/cpme.c
@@ -0,0 +1,33 @@
+#include <Z80.h>
+#include <stdio.h>
+
+
+static zuint8 cpu_read(void *context, zuint16 address)
+	{
+	return 0;
+	}
+
+
+static void cpu_write(void *context, zuint16 address, zuint8 value)
+	{
+	}
+
+
+static zuint8 cpu_in(void *context, zuint16 port)
+	{
+	return 0;
+	}
+
+
+static void cpu_out(void *context, zuint16 port, zuint8 value)
+	{
+	}
+
+
+int main(int argc, char **argv)
+	{
+	return 0;
+	}
+
+
+/* cpme.c EOF */
