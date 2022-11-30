c3bcdd5a5b3e45309915ae202c0d1e9cf267c991
redcode <manuel@zxe.io>
2022-11-30 15:14:44 +0100

    Z80 initialization.

--- sources/cpme.c
+++ sources/cpme.c
@@ -1,5 +1,8 @@
 #include <Z80.h>
+#include <Z/constants/pointer.h>
 #include <stdio.h>
+
+static Z80 cpu;
 
 
 static zuint8 cpu_read(void *context, zuint16 address)
@@ -26,6 +29,28 @@ static void cpu_out(void *context, zuint16 port, zuint8 value)
 
 int main(int argc, char **argv)
 	{
+	cpu.context	 = Z_NULL;
+	cpu.fetch_opcode =
+	cpu.fetch	 =
+	cpu.nop		 =
+	cpu.read	 = (Z80Read )cpu_read;
+	cpu.write	 = (Z80Write)cpu_write;
+	cpu.in		 = (Z80Read )cpu_in;
+	cpu.out		 = (Z80Write)cpu_out;
+	cpu.halt	 = Z_NULL;
+	cpu.nmia	 = Z_NULL;
+	cpu.inta	 = Z_NULL;
+	cpu.int_fetch    = Z_NULL;
+	cpu.ld_i_a       =
+	cpu.ld_r_a       =
+	cpu.reti	 =
+	cpu.retn	 = Z_NULL;
+	cpu.hook	 = Z_NULL;
+	cpu.illegal      = Z_NULL;
+	cpu.options      = Z80_MODEL_ZILOG_NMOS;
+
+	z80_power(&cpu, TRUE);
+
 	return 0;
 	}
 
